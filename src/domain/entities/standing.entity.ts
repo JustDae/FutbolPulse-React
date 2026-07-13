c76ab51d0deefb3fd218ffd19987f4cb4051517c
SA-1078 <santiagocolimba10@gmail.com>
2026-07-13 17:33:20 -0500

    style(player): aplica espaciado en entidad Standing

--- src/domain/entities/standing.entity.ts
+++ src/domain/entities/standing.entity.ts
@@ -3,10 +3,12 @@ export interface Standing {
   teamName: string;
   teamLogo?: string;
   tournamentId?: string;
+
   played: number;
   won: number;
   drawn: number;
   lost: number;
+
   goalsFor: number;
   goalsAgainst: number;
   goalDifference: number;
